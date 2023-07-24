fe9f121378f3e6d3e974573d6e10b4fe38286da7
Chandra <ardhyachandrax@gmail.com>
2023-07-24 12:58:32 +0700

    init components gap & button

---
+++ src/components/atoms/gap/index.js
@@ -0,0 +1,6 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function Gap({height, width}) {
+  return <View style={{height: height, width: width}} />;
+}
---
+++ src/components/atoms/index.js
@@ -0,0 +1,4 @@
+import Button from './buttons';
+import Gap from './gap';
+
+export {Button, Gap};
