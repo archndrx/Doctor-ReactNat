84004e0fe1dfb46f5ee6fba9cfcdf6b80fbe1d16
Chandra <ardhyachandrax@gmail.com>
2023-07-24 10:59:33 +0700

    create splash screen

---
+++ src/pages/splash/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {ILLogo} from '../../assets';
+
+const Splash = () => (
+  <View style={styles.page}>
+    <ILLogo />
+    <Text style={styles.title}>My Doctor</Text>
+  </View>
+);
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {fontSize: 20, fontWeight: '600', color: '#112340', marginTop: 20},
+});
