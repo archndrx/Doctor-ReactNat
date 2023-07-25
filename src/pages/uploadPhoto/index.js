e2ebb7f79fab51b173dc13cba0e418d0f52290af
Chandra <ardhyachandrax@gmail.com>
2023-07-25 09:46:39 +0700

    slicing upload photo page

---
+++ src/pages/uploadPhoto/index.js
@@ -0,0 +1,76 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Button, Gap, Header, LinkText} from '../../components';
+import {ICAddPhoto, ILUserPhotoNull} from '../../assets';
+import {colors, fonts} from '../../utils';
+
+export default function UploadPhoto({navigation}) {
+  return (
+    <View style={styles.page}>
+      <Header title={'Upload Photo'} onPress={() => navigation.goBack()} />
+      <View style={styles.content}>
+        <View style={styles.profile}>
+          <View style={styles.avatarWrapper}>
+            <Image source={ILUserPhotoNull} style={styles.avatar} />
+            <ICAddPhoto style={styles.addPhoto} />
+          </View>
+          <Text style={styles.nameText}>Shayna Melinda</Text>
+          <Text style={styles.jobText}>Product Designer</Text>
+        </View>
+        <View>
+          <Button title={'Upload and Continue'} />
+          <Gap height={30} />
+          <LinkText text={'Skip for this'} size={16} align={'center'} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  content: {
+    paddingHorizontal: 40,
+    paddingBottom: 64,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  avatar: {
+    width: 110,
+    height: 110,
+  },
+  avatarWrapper: {
+    width: 130,
+    height: 130,
+    borderWidth: 1,
+    borderColor: colors.border,
+    borderRadius: 130 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profile: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  addPhoto: {
+    position: 'absolute',
+    bottom: 8,
+    right: 6,
+  },
+  nameText: {
+    fontSize: 24,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    textAlign: 'center',
+  },
+  jobText: {
+    fontSize: 18,
+    fontFamily: fonts.primary[400],
+    color: colors.text.secondary,
+    textAlign: 'center',
+    marginTop: 4,
+  },
+});
