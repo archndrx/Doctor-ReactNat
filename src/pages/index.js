65fcdd7c41bbf2256033d4ed7673bbd9fe6be112
Chandra <ardhyachandrax@gmail.com>
2023-07-25 10:04:14 +0700

    add doctors, messages, hospitals page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,17 @@ import GetStarted from './getStarted';
 import Register from './register';
 import Login from './login';
 import UploadPhoto from './uploadPhoto';
+import Doctors from './doctors';
+import Messages from './messages';
+import Hospitals from './hospitals';
 
-export {Splash, GetStarted, Register, Login, UploadPhoto};
+export {
+  Splash,
+  GetStarted,
+  Register,
+  Login,
+  UploadPhoto,
+  Doctors,
+  Messages,
+  Hospitals,
+};
