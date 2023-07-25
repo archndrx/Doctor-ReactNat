0991f69959c8eeef00a5676aafd233b78deaafa5
Chandra <ardhyachandrax@gmail.com>
2023-07-25 10:03:39 +0700

    prettier things

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,5 +1,5 @@
 import ICBackArrow from './arrow_back.svg';
-import ICAddPhoto from './btn_add_photo.svg'
-import ICRemovePhoto from './btn_remove_photo.svg'
+import ICAddPhoto from './btn_add_photo.svg';
+import ICRemovePhoto from './btn_remove_photo.svg';
 
 export {ICBackArrow, ICAddPhoto, ICRemovePhoto};
--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,5 @@
 import ILLogo from './logo.svg';
 import ILBg from './bg.png';
-import ILUserPhotoNull from './user_photo_null.png'
+import ILUserPhotoNull from './user_photo_null.png';
 
 export {ILLogo, ILBg, ILUserPhotoNull};
