ec77cc7390b0ef09eb5086d4088f4aa65c4fba59
Chandra <ardhyachandrax@gmail.com>
2023-07-25 21:08:29 +0700

    add assets

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,5 +1,21 @@
 import ICBackArrow from './arrow_back.svg';
 import ICAddPhoto from './btn_add_photo.svg';
 import ICRemovePhoto from './btn_remove_photo.svg';
+import ICDoctors from './ic-doctors.svg';
+import ICDoctorsActive from './ic-doctors-active.svg';
+import ICMessages from './ic-messages.svg';
+import ICMessagesActive from './ic-messages-active.svg';
+import ICHospitals from './ic-hospitals.svg';
+import ICHospitalsActive from './ic-hospitals-active.svg';
 
-export {ICBackArrow, ICAddPhoto, ICRemovePhoto};
+export {
+  ICBackArrow,
+  ICAddPhoto,
+  ICRemovePhoto,
+  ICDoctors,
+  ICDoctorsActive,
+  ICHospitals,
+  ICHospitalsActive,
+  ICMessages,
+  ICMessagesActive,
+};
