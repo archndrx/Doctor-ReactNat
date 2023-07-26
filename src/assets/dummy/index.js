504300a1dbbe05e37eccf8b8f653c14fdd8a6733
Chandra <ardhyachandrax@gmail.com>
2023-07-26 10:25:00 +0700

    add dummy assets

--- src/assets/dummy/index.js
+++ src/assets/dummy/index.js
@@ -5,5 +5,19 @@ import DumDok3 from './dok3.png';
 import DumNews1 from './news1.png';
 import DumNews2 from './news2.png';
 import DumNews3 from './news3.png';
+import DumHos1 from './hos1.png';
+import DumHos2 from './hos2.png';
+import DumHos3 from './hos3.png';
 
-export {DumUser, DumDok1, DumDok2, DumDok3, DumNews1, DumNews2, DumNews3};
+export {
+  DumUser,
+  DumDok1,
+  DumDok2,
+  DumDok3,
+  DumNews1,
+  DumNews2,
+  DumNews3,
+  DumHos1,
+  DumHos2,
+  DumHos3,
+};
