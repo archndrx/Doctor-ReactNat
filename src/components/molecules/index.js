3e12ccd26e3a6baef6375fa4372f5d5a86166119
Chandra <ardhyachandrax@gmail.com>
2023-07-26 09:28:50 +0700

    add component for doctors page

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,15 @@
 import Header from './header';
 import BottomNavigator from './bottomNavigator';
 import HomeProfile from './homeProfile';
+import DoctorCategory from './doctorCategory';
+import RatedDoctor from './ratedDoctors';
+import NewsItem from './newsItem';
 
-export {Header, BottomNavigator, HomeProfile};
+export {
+  Header,
+  BottomNavigator,
+  HomeProfile,
+  DoctorCategory,
+  RatedDoctor,
+  NewsItem,
+};
