620a50c326c29db121ef11683e8130b322945162
Chandra <ardhyachandrax@gmail.com>
2023-07-26 09:52:22 +0700

    slicing messages page

--- src/pages/messages/index.js
+++ src/pages/messages/index.js
@@ -1,12 +1,39 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import { ListDoctors } from '../../components';
+import { colors, fonts } from '../../utils';
 
 export default function Messages() {
   return (
-    <View>
-      <Text>Messages</Text>
+    <View style={styles.page}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Messages</Text>
+      <ListDoctors/>
+      <ListDoctors/>
+      <ListDoctors/>
+      </View>
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    
+    backgroundColor: colors.secondary,
+    flex:1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius:20,
+  },
+  title: {
+    fontSize: 20,
+    color: colors.text.primary,
+    fontFamily: fonts.primary[600],
+    marginTop: 30,
+    marginLeft:16,
+  },
+});
