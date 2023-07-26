3fd1cac430796a41d282dd78e20984f48d7a4a12
Chandra <ardhyachandrax@gmail.com>
2023-07-26 09:29:13 +0700

    add component doctorCategory

---
+++ src/components/molecules/doctorCategory/index.js
@@ -0,0 +1,39 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ILCatDokUmum} from '../../../assets';
+import {colors, fonts} from '../../../utils';
+
+export default function DoctorCategory() {
+  return (
+    <View style={styles.container}>
+      <ILCatDokUmum style={styles.illustration} />
+      <Text style={styles.label}>Saya butuh </Text>
+      <Text style={styles.category}>Dokter Umum</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    backgroundColor: colors.cardLight,
+    alignSelf: 'flex-start',
+    borderRadius: 10,
+    marginRight: 10,
+    width: 110,
+    height: 130,
+  },
+  illustration: {
+    marginBottom: 28,
+  },
+  label: {
+    fontSize: 12,
+    fontFamily: fonts.primary[300],
+    color: colors.text.primary,
+  },
+  category: {
+    fontSize: 12,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+  },
+});
