767c907c47be3fa57f385999c4c7279695d7fb80
Chandra <ardhyachandrax@gmail.com>
2023-07-27 21:34:50 +0700

    header type adjusment

--- src/components/molecules/header/index.js
+++ src/components/molecules/header/index.js
@@ -3,28 +3,35 @@ import React from 'react';
 import {Button, Gap} from '../../atoms';
 import {colors, fonts} from '../../../utils';
 
-export default function Header({onPress, title}) {
+export default function Header({onPress, title, type}) {
   return (
-    <View style={styles.container}>
-      <Button type={'icon-only'} icon={'back-dark'} onPress={onPress} />
-      <Text style={styles.text}>{title}</Text>
+    <View style={styles.container(type)}>
+      <Button
+        type={'icon-only'}
+        icon={type === 'dark' ? 'back-light' : 'back-dark'}
+        onPress={onPress}
+      />
+      <Text style={styles.text(type)}>{title}</Text>
       <Gap width={24} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: type => ({
     paddingHorizontal: 16,
     paddingVertical: 32,
+    backgroundColor: type === 'dark' ? colors.secondary : colors.white,
     flexDirection: 'row',
     alignItems: 'center',
-  },
-  text: {
+    borderBottomLeftRadius: type === 'dark' ? 20 : 0,
+    borderBottomRightRadius: type === 'dark' ? 20 : 0,
+  }),
+  text: type => ({
     fontSize: 20,
-    color: colors.text.primary,
+    color: type === 'dark' ? colors.white : colors.text.primary,
     fontFamily: fonts.primary[600],
     textAlign: 'center',
     flex: 1,
-  },
+  }),
 });
