010cb9b9545837a0150ae5aa7dc98fc8a70e6673
Chandra <ardhyachandrax@gmail.com>
2023-07-27 21:32:39 +0700

    add new icon assets

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,4 +1,5 @@
 import ICBackArrow from './arrow_back.svg';
+import ICBackArrowLight from './arrow_back_light.svg';
 import ICAddPhoto from './btn_add_photo.svg';
 import ICRemovePhoto from './btn_remove_photo.svg';
 import ICDoctors from './ic-doctors.svg';
@@ -8,9 +9,12 @@ import ICMessagesActive from './ic-messages-active.svg';
 import ICHospitals from './ic-hospitals.svg';
 import ICHospitalsActive from './ic-hospitals-active.svg';
 import ICStar from './star.svg';
+import ICNextArrow from './arrow_right.svg';
 
 export {
   ICBackArrow,
+  ICNextArrow,
+  ICBackArrowLight,
   ICAddPhoto,
   ICRemovePhoto,
   ICDoctors,
