591d9ece4c131a1a09df84f67221c6cf1b0ce4b5
Chandra <ardhyachandrax@gmail.com>
2023-07-29 13:43:14 +0700

    slicing doctorProfile pages

---
+++ src/pages/doctorProfile/index.js
@@ -0,0 +1,34 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Button, Gap, Header, Profile, ProfileItem} from '../../components';
+import {colors} from '../../utils';
+
+export default function DoctorProfile({navigation}) {
+  return (
+    <View style={styles.page}>
+      <Header title={'Doctor Profile'} onPress={() => navigation.goBack()} />
+      <Profile name={'Alexa Rachel'} desc={'Pediatrist'} />
+      <Gap height={10} />
+      <ProfileItem label={'Alumnus'} desc={'Universitas Indonesia, 2020'} />
+      <ProfileItem
+        label={'Tempat Praktik'}
+        desc={'Rumah Sakit Umum, Bandung'}
+      />
+      <ProfileItem label={'No. STR'} desc={'092190410171'} />
+      <View style={styles.buttonAction}>
+        <Button title={'Start Consultation'} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.white,
+    flex: 1,
+  },
+  buttonAction: {
+    paddingHorizontal: 40,
+    paddingTop: 23,
+  },
+});
