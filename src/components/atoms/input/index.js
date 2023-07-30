bf4d4b53306a97a2e8ea172ce49accb59921981c
Chandra <ardhyachandrax@gmail.com>
2023-07-30 13:33:37 +0700

    add focus & blur to input

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -1,23 +1,34 @@
 import {StyleSheet, Text, TextInput, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {colors, fonts} from '../../../utils';
 
 export default function Input({label}) {
+  const [border, setBorder] = useState(colors.border);
+  const onFocusForm = () => {
+    setBorder(colors.tertiary);
+  };
+  const onBlurForm = () => {
+    setBorder(colors.border);
+  };
   return (
     <View>
       <Text style={styles.label}>{label}</Text>
-      <TextInput style={styles.input} />
+      <TextInput
+        onFocus={onFocusForm}
+        onBlur={onBlurForm}
+        style={styles.input(border)}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
+  input: border => ({
     padding: 12,
     borderWidth: 1,
     borderRadius: 10,
-    borderColor: colors.border,
-  },
+    borderColor: border,
+  }),
   label: {
     fontSize: 16,
     marginBottom: 6,
